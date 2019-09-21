2b03889f6775f76e5b75678ad6af37e54fb6fd7e
kiamco <kiamcoo@gmail.com>
2019-09-21 12:59:28 -0700

    added some style

--- src/components/todoCard.js
+++ src/components/todoCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const TodoCart = (props) => {
-    return(
-        <div className='card-container'>
-            <h1>{props.item}</h1>
-        </div>
+    return ( <
+        div className = 'list-item' >
+        <
+        h1 > { props.item } < /h1> <
+        /div>
     )
 }
 
