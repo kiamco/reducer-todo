2cdab83594292690205fa2531ca8873ff0811246
kiamco <kiamcoo@gmail.com>
2019-09-21 12:46:34 -0700

    working list

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,16 +1,15 @@
-import React, { usestate, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import TodoData from './data';
 import TodoCard from './todoCard';
 
 const TodoList = (props) => {
-    const [todos, setTodos] = usestate(TodoData);
+    const [todos, setTodos] = useState(TodoData);
 
     return(
         <div className='list-container'>
             {
                 todos.map(el => <TodoCard item={el.item} isCompleted={el.completed} />)
             }
-
         </div>
     )
 }
